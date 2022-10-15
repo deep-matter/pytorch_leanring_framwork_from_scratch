deac5731b392c6c8814e91938b9226adfbf5a462
@Youness_Elbrag <younsselbrag@gmail.com>
2022-10-15 22:25:13 +0300

    class empty to save tensor

--- tensor_operations.py
+++ tensor_operations.py
@@ -6,11 +6,19 @@ import numpy as np
 
 
 class EmptyOut:
+
     def __init__(self,outs):
 
-        self.empty = torch.empty(3,out=outs)
+        self.empty_1D = torch.empty(3,out=outs)
+          
+        return print(f"final outpist of empyt list :{self.empty_1D}")
+   
 
-        return print(f"final outpist of empyt list :{self.empty}")
+    def dim_n(self,dim):
+
+        empty_2D=torch.empty(size=dim)
+
+        return print(f"results of matrix size dim {empty_2D}")
 
 
 
@@ -28,7 +36,7 @@ empty_tensor = EmptyOut(outs)
 power_tensor=tensor_x.pow(2)
 empty_tensor=EmptyOut(power_tensor)
 
-## inplace tensor 
+# ## inplace tensor 
 
 inplace = tensor_x.add_(tensor_x)
 print(inplace)
@@ -36,5 +44,19 @@ print(inplace)
 tensor_x += tensor_x
 empty_tensor=EmptyOut(tensor_x)
 
+
+####################
+# matrices multiplications 
+###################
+
+matrix_x=torch.rand(3,3)
+matrix_y=torch.empty(3,3).uniform_(1,2)
+
+mul_matrix=torch.mm(matrix_x,matrix_y)
+
+results=EmptyOut(mul_matrix)
+results.dim_n((3,3))
+
+#print(results)
 
 
