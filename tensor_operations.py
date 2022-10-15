29f8f62ce7bf6047eac342b92c048f4a8468f9e7
@Youness_Elbrag <younsselbrag@gmail.com>
2022-10-15 21:24:37 +0300

    diffrenr way to do oprarions

--- tensor_operations.py
+++ tensor_operations.py
@@ -4,10 +4,37 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
-tensor_x = torch.tensor([1,2,3])
-tensor_y = torch.tensor([1,5,4])
+
+class EmptyOut:
+    def __init__(self,outs):
+
+        self.empty = torch.empty(3,out=outs)
+
+        return print(f"final outpist of empyt list :{self.empty}")
 
 
-empty_tensor=torch.empty(3)
-print(torch.add(tensor_x,tensor_y))
+
+# additional two 1D matrix
+tensor_x = torch.tensor([1, 2, 3])
+tensor_y = torch.tensor([1, 5, 4])
+
+outs=torch.add(tensor_x,tensor_y)
+empty_tensor=EmptyOut(outs)
+
+# multiplicatio 1D tensor
+outs = torch.mul(tensor_x, tensor_y)
+empty_tensor = EmptyOut(outs)
+
+power_tensor=tensor_x.pow(2)
+empty_tensor=EmptyOut(power_tensor)
+
+## inplace tensor 
+
+inplace = tensor_x.add_(tensor_x)
+print(inplace)
+## other method 
+tensor_x += tensor_x
+empty_tensor=EmptyOut(tensor_x)
+
+
 
